05bd4868da4333da8b8be0389eb5d6dd4a9cbdc7
Imalka Sewwandi <imalkamanathung03@gmail.com>
2026-02-01 17:08:35 +0530

    Add user model class

---
+++ src/main/java/com/hotelbillingsystem/utils/HashPassword.java
@@ -0,0 +1,24 @@
+package com.hotelbillingsystem.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashPassword {
+
+
+    public static String hashPassword(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hashedBytes = md.digest(password.getBytes(StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashedBytes) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
