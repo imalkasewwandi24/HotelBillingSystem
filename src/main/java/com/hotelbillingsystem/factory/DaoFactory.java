0c33dcb5f76fa9e308e47bde10e6e0e50f64bcee
Imalka Sewwandi <imalkamanathung03@gmail.com>
2026-02-05 19:26:57 +0530

    Add ReservationService and update DAOFactory

--- src/main/java/com/hotelbillingsystem/factory/DaoFactory.java
+++ src/main/java/com/hotelbillingsystem/factory/DaoFactory.java
@@ -1,13 +1,18 @@
 package com.hotelbillingsystem.factory;
 
 import com.hotelbillingsystem.dao.UserDao;
-
+import com.hotelbillingsystem.dao.ReservationDao;
 
 public class DaoFactory {
 
 
-    public static UserDao createUserDao() {
+    public static UserDao createUserDao()
+    {
         return new UserDao();
+    }
+    public static ReservationDao createReservationDao()
+    {
+        return new ReservationDao();
     }
 
 }
